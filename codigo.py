0a165b6f076d72d5c66cc4e93d972872fe0f648b
agustin <agussilvaa2002@gmail.com>
2026-04-22 23:44:48 -0300

    dev b: cambio en codigo

--- codigo.py
+++ codigo.py
@@ -3,4 +3,8 @@ apellido = "Dalto"
 
 saludo = "Hola "+ nombre + " " + apellido + " ¿Como estas?"
 print(saludo)
+<<<<<<< HEAD
 cambio desde DEV remoto
+=======
+ cambio desde DEV B
+>>>>>>> f83e (dev b: cambio en codigo)
